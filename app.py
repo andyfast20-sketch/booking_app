e8afaaabdc1b9abb87704994d21ae6e204713a93
andyfast20-sketch <andyfast20@gmail.com>
2025-10-27 21:39:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def book():
 if __name__ == '__main__':
     # host="0.0.0.0" lets it work online later too
     app.run(host="0.0.0.0", port=5000, debug=True)
-Fix CORS to allow Neocities access
+
+
 
 
